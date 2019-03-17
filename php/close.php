97c8d01c63118bf146fe5630a8b646d73e4365be
MVC <michael@colianna.net>
2019-03-17 03:02:52 -0400

    Updates the closing process.

--- php/close.php
+++ php/close.php
@@ -13,10 +13,10 @@ if (!isset($_COOKIE['wywo_user']))
 if( is_numeric( $_GET['id'] ) ) {
 
 // The call ID.
-	$call = mysql_fetch_assoc( mysql_query( "SELECT * FROM `calls` WHERE `id`='" . $_GET['id'] . "'" ) );
+	$call = mysqli_fetch_assoc($mysqli->query("SELECT * FROM `calls` WHERE `id`='" . $_GET['id'] . "'"));
 
 // Employee who closed the call.
-	$employee = mysql_real_escape_string( $_COOKIE['wywo_user'] );
+	$employee = $mysqli->real_escape_string($_COOKIE['wywo_user']);
 	$password = $_COOKIE['wywo_pass'];
 
 // Get employee info to see if the person is allowed to remove calls.
@@ -30,29 +30,38 @@ if( is_numeric( $_GET['id'] ) ) {
     #}
 
 // Starts the 'info' block with the primary number.
-	$info = mysql_real_escape_string( phone_text( $call['primary'] ) );
+	$info = $mysqli->real_escape_string(phone_text($call['primary']));
 
 // Adds the secondary number if it's there.
-	if( $call['secondary'] ) { $info.= ' / ' . mysql_real_escape_string( phone_text( $call['secondary'] ) ); }
+    if ($call['secondary'])
+    {
+        $info.= ' / ' . $mysqli->real_escape_string(phone_text($call['secondary']));
+    }
 
 // Adds the repair number if it's there.
-	if( $call['repair'] ) { $info.= ' / ' . mysql_real_escape_string( $call['repair'] ); }
+    if ($call['repair'])
+    {
+        $info.= ' / ' . $mysqli->real_escape_string($call['repair']);
+    }
 
 // Pulls the notes, loops through them, and puts them in a block.
-	$notes_pull = mysql_query( "SELECT * FROM `notes` WHERE `call`='" . $call['id'] . "'" );
-	while( $note = mysql_fetch_assoc( $notes_pull ) ) { $notes.= mysql_real_escape_string( date( 'm/d/Y', $note['timestamp'] ) ) . ' at ' . mysql_real_escape_string( date( 'g:ia', $note['timestamp'] ) ) . ' by ' . mysql_real_escape_string( $note['employee'] ) . ': ' . mysql_real_escape_string( str_replace( "\n", '', $note['content'] ) ) . "\n"; }
+	$notes_pull = $mysqli->query("SELECT * FROM `notes` WHERE `call`='" . $call['id'] . "'");
+    while ($note = mysqli_fetch_assoc($notes_pull))
+    {
+        $notes.= $mysqli->real_escape_string(date('m/d/Y', $note['timestamp'])) . ' at ' . $mysqli->real_escape_string(date('g:ia', $note['timestamp'])) . ' by ' . $mysqli->real_escape_string($note['employee']) . ': ' . $mysqli->real_escape_string(str_replace("\n", '', $note['content'])) . "\n";
+    }
 
 //	Insert the closed call into the archive, delete it, and delete all notes associated with it.
-	mysql_query( "INSERT INTO `archive` (`opened`, `closed`, `employee`, `type`, `customer`, `info`, `notes`) VALUES ('" . $call['timestamp'] . "', '" . time() . "', '" . $employee . "', '" . mysql_real_escape_string( $call['type'] ) . "', '" . mysql_real_escape_string( $call['customer'] ) . "', '" . $info . "', '" . $notes . "')" );
-	mysql_query( "DELETE FROM `calls` WHERE `id`='" . $call['id'] . "'" );
-	mysql_query( "DELETE FROM `notes` WHERE `call`='" . $call['id'] . "'" );
+	$mysqli->query("INSERT INTO `archive` (`opened`, `closed`, `employee`, `type`, `customer`, `info`, `notes`) VALUES ('" . $call['timestamp'] . "', '" . time() . "', '" . $employee . "', '" . $mysqli->real_escape_string($call['type']) . "', '" . $mysqli->real_escape_string($call['customer']) . "', '" . $info . "', '" . $notes . "')");
+	$mysqli->query("DELETE FROM `calls` WHERE `id`='" . $call['id'] . "'");
+	$mysqli->query("DELETE FROM `notes` WHERE `call`='" . $call['id'] . "'");
 
 }
 
 #	-----	Optimize tables and redirect.
-mysql_query( "OPTIMIZE TABLE `archive`" );
-mysql_query( "OPTIMIZE TABLE `calls`" );
-mysql_query( "OPTIMIZE TABLE `notes`" );
+$mysqli->query("OPTIMIZE TABLE `archive`");
+$mysqli->query("OPTIMIZE TABLE `calls`");
+$mysqli->query("OPTIMIZE TABLE `notes`");
 header('Location:./');
 exit();
 
