13a4761fd21163f62582c746ee8f56d4f229b97f
MVC <michael@colianna.net>
2019-03-17 02:11:30 -0400

    Updates the header function and the call counts in the basic file.

--- php/basic.php
+++ php/basic.php
@@ -2,19 +2,15 @@
 		//	Script: WYWO common functions
 		//	Revision: 3.0.6
 
-#	-----	Connection to the SQL database.
-mysql_connect( 'localhost', 'wywo', 'r122!wywo' );
-mysql_select_db( 'wywo' );
-
 #	-----	Cache control for Snow Leopard server.
-header( 'Cache-Control: no-cache, must-revalidate' );
-header( 'Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
 
 #	-----	Gets the tallies for the types of calls.
-$all_calls = mysql_num_rows( mysql_query( "SELECT * FROM `calls`" ) );
-$genius_calls = mysql_num_rows( mysql_query( "SELECT * FROM `calls` WHERE `type`='genius'" ) );
-$business_calls = mysql_num_rows( mysql_query( "SELECT * FROM `calls` WHERE `type`='business'" ) );
-$manager_calls = mysql_num_rows( mysql_query( "SELECT * FROM `calls` WHERE `type`='manager'" ) );
+$all_calls = mysqli_num_rows($mysqli->query("SELECT * FROM `calls`"));
+$genius_calls = mysqli_num_rows($mysqli->query("SELECT * FROM `calls` WHERE `type`='genius'"));
+$business_calls = mysqli_num_rows($mysqli->query("SELECT * FROM `calls` WHERE `type`='business'"));
+$manager_calls = mysqli_num_rows($mysqli->query("SELECT * FROM `calls` WHERE `type`='manager'"));
 
 #	-----	The text for the "login/logout" link.
 if( $_COOKIE['wywo_user'] ) { $crumbs = 'Log Out (' . $_COOKIE['wywo_user'] . ')'; }
